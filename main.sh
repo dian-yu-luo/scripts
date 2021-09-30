fa746404c430a107a4ff223963524688eabe11a8
dianyuluo <1739556970@qq.com>
2021-09-30 14:52:03 +0000

    totally config

--- main.sh
+++ main.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-bash 
+mkdir temp 
+cd temp/
+wget https://raw.githubusercontent.com/dian-yu-luo/scripts/main/apt.sh 
+wget https://raw.githubusercontent.com/dian-yu-luo/scripts/main/adduser.sh  
+wget https://raw.githubusercontent.com/dian-yu-luo/scripts/main/getconfig.sh 
+
+bash apt.sh 
+bash adduser.sh 
+bash getconfig.sh 
