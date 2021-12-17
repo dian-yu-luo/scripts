00041d547332fe7bb623e919e2162e5520d24af1
dianyuluo <1739556970@qq.com>
2021-12-17 08:05:33 +0000

    修正了main函数，让main.sh 可以真的单独执行

--- main.sh
+++ main.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
-mkdir temp 
-cd temp/
-wget https://raw.githubusercontent.com/dian-yu-luo/scripts/main/apt.sh 
-wget https://raw.githubusercontent.com/dian-yu-luo/scripts/main/adduser.sh  
-wget https://raw.githubusercontent.com/dian-yu-luo/scripts/main/getconfig.sh 
+userPath=$HOME
 
-bash apt.sh 
-bash adduser.sh 
-bash getconfig.sh 
+echo "do you want add a user (Y/N)"
+read isCheck
+if [ -z $isCheck ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+    echo "input your name"
+    read username
+    # 读取用户名，并把用户名翻到username里
+    if sudo useradd -d /home/$username -m $username -s /bin/bash; then
+        echo "ok,and then input you password "
+    else
+        echo "you need sudo permission"
+        exit
+    fi
+    sudo passwd $username
+fi
+
+echo "do you want mkdir this user have sudo privalige?(Y/N)"
+read isCheck
+if [ -z $isCheck ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+    echo "ok I will do that for you "
+    sudo sed -i "21 i\ ${username}	ALL=(ALL:ALL) ALL" /etc/sudoers
+fi
+
+echo "do you want add git config?(Y/N)"
+read isCheck
+if [ -z $isCheck ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+    git config --global user.name "dianyuluo"
+    git config --global user.email 1739556970@qq.com
+    git config --global credential.helper store
+fi
+
+echo "do you want add VIM config?(Y/N)"
+read isCheck
+if [ -z $isCheck ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+    sudo apt update && sudo apt upgrade && sudo apt install wget vim
+    wget -P $userPath/ https://github.com/dian-yu-luo/scripts/blob/main/conf/.vimrc
+fi
