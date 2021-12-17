07f2d017bdd0c633b15f8d660473e9d79a3b72a7
dianyuluo <1739556970@qq.com>
2021-12-17 08:08:59 +0000

    学习脚本配置过程

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ userPath=$HOME
 
 echo "do you want add a user (Y/N)"
 read isCheck
-if [ -z $username ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+if [ -z $isCheck ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
     echo "input your name"
     read username
     # 读取用户名，并把用户名翻到username里
@@ -18,14 +18,14 @@ fi
 
 echo "do you want mkdir this user have sudo privalige?(Y/N)"
 read isCheck
-if [ -z $username ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+if [ -z $isCheck ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
     echo "ok I will do that for you "
     sudo sed -i "21 i\ ${username}	ALL=(ALL:ALL) ALL" /etc/sudoers
 fi
 
 echo "do you want add git config?(Y/N)"
 read isCheck
-if [ -z $username ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+if [ -z $isCheck ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
     git config --global user.name "dianyuluo"
     git config --global user.email 1739556970@qq.com
     git config --global credential.helper store
@@ -33,7 +33,7 @@ fi
 
 echo "do you want add VIM config?(Y/N)"
 read isCheck
-if [ -z $username ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
-    sudo apt update && sudo apt upgrade &&sudo apt install wget vim 
+if [ -z $isCheck ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+    sudo apt update && sudo apt upgrade && sudo apt install wget vim
     wget -P $userPath/ https://github.com/dian-yu-luo/scripts/blob/main/conf/.vimrc
 fi
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 userPath=$HOME
 echo "do you want add VIM config?(Y/N)"
 read isCheck
-if [ -z $username ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+if [ -z $isCheck ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
     sudo apt update && sudo apt upgrade && sudo apt install vim wget -y
     wget -P $userPath/ https://raw.githubusercontent.com/dian-yu-luo/scripts/main/conf/.vimrc
 fi
