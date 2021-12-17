d6bad16db0d0dd37aefa924c985cd0faa68819aa
dianyuluo <1739556970@qq.com>
2021-12-17 07:56:16 +0000

    check

--- main.sh
+++ main.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
-
-isCheckY=y
-isCheckN=n
+userPath=$HOME
 
 echo "do you want add a user (Y/N)"
 read isCheck
-if [ $isCheck = y ] || [ $isCheck = Y ]; then
+if [ -z $username ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
     echo "input your name"
     read username
     # 读取用户名，并把用户名翻到username里
-    if useradd -d /home/$username -m $username -s /bin/bash; then
+    if sudo useradd -d /home/$username -m $username -s /bin/bash; then
         echo "ok,and then input you password "
     else
         echo "you need sudo permission"
         exit
     fi
-    passwd $username
+    sudo passwd $username
 fi
 
-echo "do you want mkdir this user have sudo privalige?"
+echo "do you want mkdir this user have sudo privalige?(Y/N)"
 read isCheck
-if [ $isCheck = y ] || [ $isCheck = Y ]; then
+if [ -z $username ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
     echo "ok I will do that for you "
-    sed -i '21 i\www	ALL=(ALL:ALL) ALL' /etc/sudoers
+    sudo sed -i "21 i\ ${username}	ALL=(ALL:ALL) ALL" /etc/sudoers
+fi
+
+echo "do you want add git config?(Y/N)"
+read isCheck
+if [ -z $username ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+    git config --global user.name "dianyuluo"
+    git config --global user.email 1739556970@qq.com
+    git config --global credential.helper store
+fi
+
+echo "do you want add VIM config?(Y/N)"
+read isCheck
+if [ -z $username ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+    sudo apt update && sudo apt upgrade &&sudo apt install wget vim 
+    wget -P $userPath/ https://github.com/dian-yu-luo/scripts/blob/main/conf/.vimrc
 fi
--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-
+userPath=$HOME
+echo "do you want add VIM config?(Y/N)"
 read isCheck
-if [ $isCheck = y ] || [ $isCheck = Y ]; then
-    echo $isCheck
+if [ -z $username ] || [ $isCheck = y ] || [ $isCheck = Y ]; then
+    sudo apt update && sudo apt upgrade && sudo apt install vim wget -y
+    wget -P $userPath/ https://raw.githubusercontent.com/dian-yu-luo/scripts/main/conf/.vimrc
 fi
