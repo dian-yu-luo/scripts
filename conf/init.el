341407fc111816ef5afb22087fba49ecd531c107
dianyuluo <1739556970@qq.com>
2022-02-08 19:28:46 +0800

    update emacs init.el

--- conf/init.el
+++ conf/init.el
@@ -8,13 +8,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil phi-autopair org-download dashboard dash org company)))
+   '(amx ivy magit evil phi-autopair org-download dashboard dash org company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Malgun Gothic" :foundry "outline" :slant normal :weight normal :height 163 :width normal)))))
+ '(default ((t (:family "楷体" :foundry "outline" :slant normal :weight normal :height 163 :width normal)))))
  
 ;; 设置行号
 (global-linum-mode 1)
@@ -23,7 +23,9 @@
 (scroll-bar-mode -1)
 
 ;; 自动补全模式
-(add-hook 'after-init-hook 'global-company-mode)
+(global-company-mode 1)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
 
 ;; 因为有了git 不需要备份文件
 (setq make-backup-files nil)
@@ -53,8 +55,11 @@
 (global-hl-line-mode 1)
 ;; 保存退出的时候的光标位置
 (save-place-mode 1)
-;; 缩进问题
-(setq-default tab-width 4)
+;; 显示文本的字符数量
+(size-indication-mode 1)
+;; 主要配置 M-x 的命令配置，提高自动补全的智能效果
+(ivy-mode 1)
+(amx-mode 1)
 ;; 
 (global-set-key (kbd "<f2>") 'open-init-file)
 
@@ -62,3 +67,6 @@
 (setq evil-disable-insert-state-bindings t)
      (require 'evil)
      (evil-mode 1)
+;; tab 使用4 space 替换
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
